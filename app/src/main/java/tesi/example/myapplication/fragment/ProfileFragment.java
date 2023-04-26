2389df65892dfabece5a5901292672bd2150912a
Sebastiano <95seba@live.it>
2023-04-27 01:33:54 +0200

    Fragment

---
+++ app/src/main/java/tesi/example/myapplication/fragment/ProfileFragment.java
@@ -0,0 +1,21 @@
+package tesi.example.myapplication.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import easy.tuto.bottomnavigationfragmentdemo.R;
+
+public class ProfileFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
