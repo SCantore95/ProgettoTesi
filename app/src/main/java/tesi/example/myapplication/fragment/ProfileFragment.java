36b52e505231a92e229453b8f74133a3f04e3c63
Sebastiano <95seba@live.it>
2024-01-18 18:19:10 +0100

    ottimizzazione codice rimuovendo la parte deprecata

--- app/src/main/java/tesi/example/myapplication/fragment/ProfileFragment.java
+++ app/src/main/java/tesi/example/myapplication/fragment/ProfileFragment.java
@@ -1,16 +1,17 @@
 package tesi.example.myapplication.fragment;
 
-import static androidx.constraintlayout.widget.ConstraintLayoutStates.TAG;
-
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.AsyncTaskLoader;
+import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,155 +19,77 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import easy.tuto.bottomnavigationfragmentdemo.R;
 
+public class ProfileFragment extends Fragment implements ItemClickListener, LoaderManager.LoaderCallbacks<List<ResultsItem>> {
 
-public class ProfileFragment extends Fragment implements ItemClickListener {
+    private static final int LOADER_ID = 1;
+    private static final String JSON_URL = "https://run.mocky.io/v3/8331fd23-68c0-44ea-95da-a7cc7602b262";
+    private static final String TAG = "ProfileFragment";
 
-    // json String
-    List<ResultsItem> attackLists;
-    RecyclerView recyclerView;
+    private List<ResultsItem> attackLists;
+    private RecyclerView recyclerView;
+    private Results resultsAdapter;
     private Context mContext;
 
-    private Results resultsAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        mContext=context;
+        mContext = context;
     }
 
-    public android.view.View onCreateView(LayoutInflater inflater, ViewGroup container,
-
-                                          Bundle savedInstanceState) {
-
-        android.view.View view = inflater.inflate(R.layout.fragment_home, container, false);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
         recyclerView = view.findViewById(R.id.recyclerView);
-
-
-
-
         attackLists = new ArrayList<>();
-        GetData getData = new GetData();
-        getData.execute();
+        LoaderManager.getInstance(this).initLoader(LOADER_ID, null, this);
         return view;
-
-    }
-
-
-
-
-public class GetData extends AsyncTask<String,String,String> {
-    private  String JSON_URL="https://run.mocky.io/v3/8331fd23-68c0-44ea-95da-a7cc7602b262";
-
-    @Override
-    protected String doInBackground(String... strings) {
-        String current="";
-        try {
-            URL url;
-            HttpURLConnection urlConnection=null;
-            try {
-                url= new URL(JSON_URL);
-                urlConnection=(HttpURLConnection) url.openConnection();
-                Log.d(TAG, "http:va ");
-                InputStream is=urlConnection.getInputStream();
-
-                InputStreamReader isr = new InputStreamReader(is);
-
-                int data= isr.read();
-                while(data != -1){
-                    current +=(char) data;
-                    data= isr.read();
-                }
-                Log.d(TAG, "doInBackground() returned: " + current);
-                return current;
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }finally {
-                if(urlConnection !=null){
-                    urlConnection.disconnect();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return current;
     }
 
     @Override
-    protected void onPostExecute(String s) {
-
-        try {
-            JSONObject jsonObject=new JSONObject(s);
-            JSONArray jsonArray = jsonObject.getJSONArray("events");
-             attackLists = new ArrayList<>();
-
-            for (int i=0;i<jsonArray.length();i++){
-
-                JSONObject jsonObject1= jsonArray.getJSONObject(i);
-                ResultsItem model= new ResultsItem();
-                model.setId(jsonObject1.getString("id"));
-                model.setDescription(jsonObject1.getString("Description"));
-                model.setOffenseType(jsonObject1.getString("Offense Type"));
-                model.setOffenceSource(jsonObject1.getString("Offence Source"));
-                model.setMagnitude(jsonObject1.getString("Magnitude"));
-                model.setSourceIPs(jsonObject1.getString("Source  IPs"));
-                model.setDestinationIPs(jsonObject1.getString("Destination IPs"));
-                model.setUsers(jsonObject1.getString("Users"));
-                model.setEvents(jsonObject1.getString("Events"));
-                model.setLogSources(jsonObject1.getString("Log Sources"));
-                model.setStartDate(jsonObject1.getString("Start Date"));
-                model.setFlows(jsonObject1.getString("Flows"));
-                model.setLastEventsFlow(jsonObject1.getString("Last Events/Flow"));
-
-                attackLists.add(model);
-
-
-            }
-
-
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        // Aggiorna i dati dell'adapter con i nuovi dati ottenuti
-
-        PutDataIntoRecyclerView(attackLists);
-
+    public Loader<List<ResultsItem>> onCreateLoader(int id, Bundle args) {
+        return new GetDataLoader(mContext, JSON_URL);
     }
-}
 
-    private  void PutDataIntoRecyclerView (List<ResultsItem> attackLists){
+    @Override
+    public void onLoadFinished(Loader<List<ResultsItem>> loader, List<ResultsItem> data) {
+        if (data != null) {
+            attackLists = data;
+            PutDataIntoRecyclerView(attackLists);
+        }
+    }
 
-         resultsAdapter = new Results(requireContext(),attackLists,this);
+    @Override
+    public void onLoaderReset(Loader<List<ResultsItem>> loader) {
+        // Implementa se necessario
+    }
+
+    private void PutDataIntoRecyclerView(List<ResultsItem> attackLists) {
+        resultsAdapter = new Results(mContext, attackLists, this);
         Log.d(TAG, "attacklist put data: " + attackLists);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager (requireContext());
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(mContext);
         recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setAdapter(resultsAdapter );
+        recyclerView.setAdapter(resultsAdapter);
         if (resultsAdapter != null) {
             resultsAdapter.updateData(attackLists);
         }
     }
+
     @Override
     public void onItemClick(Intent intent) {
-        // Usa il FragmentManager per avviare il nuovo fragment con l'Intent
-        // Esempio:
         if (getActivity() != null) {
             getActivity().getSupportFragmentManager()
                     .beginTransaction()
@@ -175,8 +98,70 @@ public class GetData extends AsyncTask<String,String,String> {
                     .commit();
         }
     }
+
+    private static class GetDataLoader extends AsyncTaskLoader<List<ResultsItem>> {
+        private final String jsonUrl;
+
+        public GetDataLoader(Context context, String jsonUrl) {
+            super(context);
+            this.jsonUrl = jsonUrl;
+        }
+
+        @Override
+        protected void onStartLoading() {
+            forceLoad();
+        }
+
+        @Override
+        public List<ResultsItem> loadInBackground() {
+            try {
+                StringBuilder result = new StringBuilder();
+                HttpURLConnection urlConnection = null;
+                try {
+                    URL url = new URL(jsonUrl);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+
+                JSONObject jsonObject = new JSONObject(result.toString());
+                JSONArray jsonArray = jsonObject.getJSONArray("events");
+                List<ResultsItem> attackLists = new ArrayList<>();
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                    ResultsItem model = new ResultsItem();
+                    model.setId(jsonObject1.getString("id"));
+                    model.setDescription(jsonObject1.getString("Description"));
+                    model.setOffenseType(jsonObject1.getString("Offense Type"));
+                    model.setOffenceSource(jsonObject1.getString("Offence Source"));
+                    model.setMagnitude(jsonObject1.getString("Magnitude"));
+                    model.setSourceIPs(jsonObject1.getString("Source  IPs"));
+                    model.setDestinationIPs(jsonObject1.getString("Destination IPs"));
+                    model.setUsers(jsonObject1.getString("Users"));
+                    model.setEvents(jsonObject1.getString("Events"));
+                    model.setLogSources(jsonObject1.getString("Log Sources"));
+                    model.setStartDate(jsonObject1.getString("Start Date"));
+                    model.setFlows(jsonObject1.getString("Flows"));
+                    model.setLastEventsFlow(jsonObject1.getString("Last Events/Flow"));
+
+                    attackLists.add(model);
+                }
+                return attackLists;
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 }
-
-
-
-
