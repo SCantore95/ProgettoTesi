cf436dc13beace5db36e3c85e32af8b65fe45ebb
Sebastiano <95seba@live.it>
2024-02-13 13:16:43 +0100

    Rimosso codice non usato

--- app/src/main/java/tesi/example/myapplication/MainActivity.java
+++ app/src/main/java/tesi/example/myapplication/MainActivity.java
@@ -23,13 +23,12 @@ import tesi.example.myapplication.fragment.ItemDetailsFragment;
 import tesi.example.myapplication.fragment.ProfileFragment;
 import tesi.example.myapplication.fragment.ResultsItem;
 import easy.tuto.bottomnavigationfragmentdemo.R;
-import tesi.example.myapplication.Interface.StatsDataListener;
 
 import androidx.fragment.app.Fragment;
 
 
 public class MainActivity extends AppCompatActivity implements
-        ProfileFragment.ProfileDataListener, StatsDataListener {
+        ProfileFragment.ProfileDataListener {
 
     private static final String TAG = "mytag";
     List<ResultsItem> attackLists;
@@ -37,15 +36,15 @@ public class MainActivity extends AppCompatActivity implements
     ConstraintLayout bottomUser;
     BottomSheetDialog bottomSheetDialog;
     BottomAppBar bottomAppBar;
-    //TextView usernameTextView;
+
     StatsFragment stats = new StatsFragment();
-   // StatsFragment stats = new StatsFragment();
+
     ProfileFragment profile = new ProfileFragment();
-    PieChartFragment pieChartFragment = new PieChartFragment();
-    BarChartFragment barChartFragment = new BarChartFragment();
+
+
     ItemDetailsFragment itemDetailsFragment = new ItemDetailsFragment();
     FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-    FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction();
+
     Bundle savedInstanceState;
 
     @Override
@@ -61,9 +60,7 @@ public class MainActivity extends AppCompatActivity implements
 
         transaction.add(R.id.fragment_container, profile, "ProfileFragment")
                 .hide(profile)
-               .add(R.id.fragment_container, stats, "StatsFragment")
-
-               // .add(R.id.fragment_container, stats, "StatsFragment")
+                .add(R.id.fragment_container, stats, "StatsFragment")
                 .hide(stats)
                 .commit();
 
@@ -73,9 +70,7 @@ public class MainActivity extends AppCompatActivity implements
                 FragmentTransaction transaction = fragmentManager.beginTransaction();
 
                 switch (item.getItemId()) {
-                    //case R.id.counter:
-                     // transaction.hide(profile).hide(stats).show(counter).commit();
-                       // break;
+
                     case R.id.profile:
                         showBottomUser();
                         transaction.hide(stats).show(profile).commit();
@@ -83,12 +78,9 @@ public class MainActivity extends AppCompatActivity implements
                     case R.id.stats:
                         hideBottomUser();
                         transaction.hide(profile).show(stats).commit();
-                        // bottomNavigationView.setVisibility(View.GONE);
+
                         break;
-                    // Add a case for BarChartFragment if necessary
-            /* case R.id.bar_chart:
-                transaction.hide(stats).show(barChartFragment).commit();
-                break; */
+
                 }
 
                 return true;
@@ -112,83 +104,8 @@ public class MainActivity extends AppCompatActivity implements
 
     }
 
-    @Override
-    public void onStatsDataAvailable(List<ResultsItem> data) {
-
-
-    }
-
-    @Override
-    public void onStatsButtonClick(int buttonId) {
-        switch (buttonId) {
-            case StatsDataListener.BUTTON_PIE_CHART:
-                // Verifica che pieChartFragment non sia nullo
-                if (pieChartFragment != null) {
-                    // Aggiorna i dati del grafico a torta nel fragment
-                    //pieChartFragment.setPieChartData(stats.getCurrentData());
-                    hideBottomNavigationView();
-
-
-                    if (!pieChartFragment.isAdded()) {
-                        transaction.add(R.id.fragment_container, pieChartFragment, "PieChartFragment");
-                        transaction.show(pieChartFragment);
-                        transaction.addToBackStack(null);
-
-                        transaction.commit();
-                    } else {
-                        // Se il fragment è già stato aggiunto, mostra semplicemente il fragment
-                        getSupportFragmentManager().beginTransaction().hide(stats).commit();
-                        transaction.add(R.id.fragment_container, pieChartFragment, "PieChartFragment");
-
-                        getSupportFragmentManager().beginTransaction().show(pieChartFragment).addToBackStack(null).commit();
-
-
-
-
-
-
-
-
-                    }
-                }
-                break;
-            case StatsDataListener.BUTTON_BAR_CHART:
-                // Verifica che pieChartFragment non sia nullo
-                if (barChartFragment != null) {
-                    // Aggiorna i dati del grafico a torta nel fragment
-                  //  barChartFragment.setBarChartData(stats.getCurrentData());
-                    hideBottomNavigationView();
-                    if (!barChartFragment.isAdded()) {
-                        transaction.add(R.id.fragment_container, barChartFragment, "BarChartFragment");
-
-                        transaction.show(barChartFragment);
-                        transaction.addToBackStack(null);
-
-                        transaction.commit();
-                    } else {
-                        // Se il fragment è già stato aggiunto, mostra semplicemente il fragment
-                        getSupportFragmentManager().beginTransaction().hide(stats).commit();
-
-                        getSupportFragmentManager().beginTransaction().show(barChartFragment).addToBackStack(null).commit();
-
-
-
-                    }
-                    break;
-
-                }
-
-        }
-    }
-
-
-
-
     public void hideBottomNavigationView() {
         bottomNavigationView.setVisibility(View.GONE);
-      //  bottomAppBar.setVisibility(View.GONE);
-
-
     }
     public void hideBottomNavigationAppBar() {
        bottomAppBar.setVisibility(View.GONE);
@@ -206,8 +123,6 @@ public class MainActivity extends AppCompatActivity implements
     public void showBottomNavigationView(){
          bottomNavigationView.setVisibility(View.VISIBLE);
 }
-
-
     @Override
     public void onBackPressed() {
         // Check if any specific fragment is visible and handle the back press accordingly
